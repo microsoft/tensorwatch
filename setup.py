a21a554f791734b87db9670a322cd1f065a82212
Shital Shah <shital@ShitalShah.com>
2019-05-28 14:58:11 -0700

    Inctremented version for few bug fixes in saliecy and tsne demos

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tensorwatch",
-    version="0.8.1",
+    version="0.8.2",
     author="Shital Shah",
     author_email="shitals@microsoft.com",
     description="Interactive Realtime Debugging and Visualization for AI",
