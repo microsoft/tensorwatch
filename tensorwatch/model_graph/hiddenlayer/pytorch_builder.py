b86b1d9c8743847f42a4c26301181461bbd458db
Jacob Alber <jaalber@microsoft.com>
2019-11-02 13:39:39 -0400

    Fix graph generation for PyTorch >= 1.2.0
    The PyTorch graph import code in hiddenlayer.py is not compatible with
      PyTorch >= 1.2.0
    
    PyTorch made a breaking change, renaming uniqueName() to debugName() in this commit:
    
    https://github.com/pytorch/pytorch/commit/f7b2778cb1db148f1c901ea08abcbfa0e0a78f44

--- tensorwatch/model_graph/hiddenlayer/pytorch_builder.py
+++ tensorwatch/model_graph/hiddenlayer/pytorch_builder.py
@@ -44,7 +44,7 @@ def pytorch_id(node):
     """Returns a unique ID for a node."""
     # After ONNX simplification, the scopeName is not unique anymore
     # so append node outputs to guarantee uniqueness
-    return node.scopeName() + "/outputs/" + "/".join([o.uniqueName() for o in node.outputs()])
+    return node.scopeName() + "/outputs/" + "/".join([str(o.unique()) for o in node.outputs()])
 
 
 def get_shape(torch_node):
